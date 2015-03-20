8a0294f487047b33488016f9538c2d0c8394caea
Swidjit <alex@swidjit.com>
2015-03-20 13:24:16 -0400

    users can comment on requests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,13 @@ class CommentsController < ApplicationController
     # Not implemented: check to see whether the user has permission to create a comment on this object
     @comment = Comment.build_from(@obj, current_user.id, @comment_hash[:body])
     if @comment.save
-      render :partial => "show_comment", :locals => { :comment => @comment }, :layout => false, :status => :created
+      puts @obj.class
+      if @obj.class == Request
+        render :partial => "show_comment_on_request", :locals => { :comment => @comment, :obj => @obj }, :layout => false, :status => :created
+
+      else
+        render :partial => "show_comment", :locals => { :comment => @comment }, :layout => false, :status => :created
+      end
     else
       render :js => "alert('error saving comment');"
     end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,5 @@ class Request < ActiveRecord::Base
 
   belongs_to :user
   has_many :suggestions
-
+  acts_as_commentable
 end
